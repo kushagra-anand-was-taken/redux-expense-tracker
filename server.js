080e4bcf74d5fe4df94651508d099a4f5debdc5e
Kushagra Anand <kushrokz95@gmail.com>
2020-07-08 13:40:34 +0530

    1st and final commit

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 connectDB();
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("api running");
-});
-
 app.use("/user", require("./routes/users"));
 app.use("/auth", require("./routes/auth"));
 app.use("/transaction", require("./routes/transaction"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
